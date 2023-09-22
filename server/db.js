f8e5351955c8fd504005a9f00df40964e0989dbd
Tan Nguyen <nguyentan1980@gmail.com>
2023-09-22 21:40:39 +0700

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,12 +2,17 @@ const Pool = require('pg').Pool;
 
 require('dotenv').config();
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  connectionString: 'postgresql://postgres:MMMAzz728g534NU8@exam-perntodo-ethan-env.cba6rpvi5fmd.us-east-1.rds.amazonaws.com:5432/perntodo',
 });
+
 
 module.exports = pool;
